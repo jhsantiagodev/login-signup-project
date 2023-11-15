843883ba5875dea9acc9475bbd76e3f9a80cb380
jhSantiagoDev <jhc.santiago420@gmail.com>
2023-11-15 13:48:35 -0600

    funcion botones efectos

---
+++ main.js
@@ -0,0 +1,21 @@
+const formOpenBtn = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector(".form_container");
+const formCloseBtn = document.querySelector(".form_close");
+const signupBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw_hide");
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+signupBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+   formContainer.classList.add("active");
+});
+
+loginBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+   formContainer.classList.remove("active");
+});
